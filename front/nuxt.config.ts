3212f78edb867a0bf8f7f1ec08035c82e1325021
gangan <gangan.education@gmail.com>
2023-06-14 22:40:03 +0900

    [add] #12: Vutify3環境構築

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,20 @@
+import vuetify from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   rootDir: "src/",
+  ssr: false,
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+    define: {
+      "process.env": false,
+    },
+  },
+  css: ["@/assets/main.scss"],
 });
